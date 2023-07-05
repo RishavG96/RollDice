239f75f96ee23f97069b79c3ba35a0a7a63ec042
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-06 04:54:45 +0530

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Rishav Gupta on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
